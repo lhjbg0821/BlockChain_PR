7b531af1cec4a0ef311fb307650a0c160a2a748a
lhjbg0821 <82081355+lhjbg0821@users.noreply.github.com>
2023-04-20 13:34:18 +0900

    콘솔에 proxy 찍힘(메타마스크때문!)

--- 230420/BLOCKCHAIN/src/App.jsx
+++ 230420/BLOCKCHAIN/src/App.jsx
@@ -1,5 +1,14 @@
+import { useEffect } from "react";
+
 function App() {
-  return <div className="bg-red-100">Hello, React</div>;
+  useEffect(() => {
+    console.log(window.ethereum);
+  }, []);
+  return (
+    <div className="bg-red-100 min-h-screen flex justify-center items-center">
+      Hello, React
+    </div>
+  );
 }
 
 export default App;
